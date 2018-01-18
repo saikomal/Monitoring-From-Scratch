bbcaf28d3145cd5e06a633ee8dd47637b2a0e113
saikomalpendela <saikomalpendela@yahoo.com>
2018-01-18 20:59:36 +0530

    select_query_in_python

---
+++ select.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import multiprocessing
+import subprocess
+import os
+from time import gmtime, strftime
+from datetime import datetime
+import MySQLdb as my
+
+db = my.connect(host="localhost",
+                user="root",
+                passwd="mysqleptp",
+                db="KLUIOT"
+                )
+
+cursor = db.cursor()
+id = "FED3"
+cursor.execute("SELECT * FROM TableRange")
+data = cursor.fetchall ()
+# print the rows
+for row in data :
+    print row[0], row[1]
+
+db.commit()   # you need to call commit() method to save
+    # your changes to the database
+    
+    
+db.close()
