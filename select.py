6e28596de141e295d3dcd0e9ebc0a61904690632
saikomalpendela <saikomalpendela@yahoo.com>
2018-01-18 21:21:47 +0530

    selectedvalues_pinged_uploaded_ToDb

--- select.py
+++ select.py
@@ -7,6 +7,19 @@ from time import gmtime, strftime
 from datetime import datetime
 import MySQLdb as my
 
+def pinger( job_q, results_q ):
+    DEVNULL = open(os.devnull,'w')
+    while True:
+        ip = job_q.get()
+        if ip is None: break
+        
+        try:
+            subprocess.check_call(['ping','-c1',ip],
+                                  stdout=DEVNULL)
+            results_q.put(ip)
+        except:
+            pass
+
 db1 = my.connect(host="localhost",
                  user="root",
                  passwd="mysqleptp",
@@ -18,10 +31,52 @@ cursor1.execute("SELECT * FROM TableRange")
 data = cursor1.fetchall()
 # print the rows
 for row in data :
-    print(row[0],row[1])
+    #print(row[0],row[1])
+    pool_size = 255
+    
+    jobs = multiprocessing.Queue()
+    results = multiprocessing.Queue()
+    
+    pool = [ multiprocessing.Process(target=pinger, args=(jobs,results))
+            for i in range(pool_size) ]
+        
+            for p in pool:
+                p.start()
+
+for i in range(1,101):
+    jobs.put(row[1]+'.{0}'.format(i))
+    
+    
+    for p in pool:
+        jobs.put(None)
+
+for p in pool:
+    p.join()
+    output = " "
+    a = str(datetime.now());
+    output += a[:19]
+    output +=" "
+    while not results.empty():
+        ip = results.get()
+        ip += " "
+        output +=ip
+#print(output)
+db = my.connect(host="localhost",
+                user="root",
+                passwd="mysqleptp",
+                db="KLUIOT"
+                )
+
+cursor = db.cursor()
+id = row[0]
+number_of_rows = cursor.execute('''insert into iptable (labno,online) VALUES(%s,%s)''',(id,output))
+db.commit()   # you need to call commit() method to save
+# your changes to the database
+
+
+    db.close()
 
 cursor1.close ()
 
 
 db1.close()
-
