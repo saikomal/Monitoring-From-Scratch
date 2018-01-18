1385447e7c2004c95af1b4eab24d756da033faf8
saikomalpendela <saikomalpendela@yahoo.com>
2018-01-18 22:45:50 +0530

    complete_with_CRUD_operations_v1

--- delete.php
+++ delete.php
@@ -1,40 +1,21 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "mysqleptp";
+$dbname = "KLUIOT";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if(isset($_POST['submit'])){
+// sql to delete a record
+$sql = "DELETE FROM TableRange WHERE labname='".$_GET['val']."'";
 
-$ip = $_POST['range'];
-$labname = $_POST['labname'];
-
-$myfile = fopen($labname.".py", "w");
-$i = 0;
-$txt = "from __future__ import print_function\nimport multiprocessing\nimport subprocess\nimport os\nfrom time import gmtime, strftime\nfrom datetime import datetime\nimport MySQLdb as my\ndef pinger( job_q, results_q ):\n\tDEVNULL = open(os.devnull,'w')\n\twhile True:\n\t\tip = job_q.get()\n\t\tif ip is None: break\n\t\ttry:\n\t\t\tsubprocess.check_call(['ping','-c1',ip],stdout=DEVNULL)\n\t\t\tresults_q.put(ip)\n\t\texcept:\n\t\t\tpass\nif __name__ == '__main__':\n\tpool_size = 255\n\tjobs = multiprocessing.Queue()\n\tresults = multiprocessing.Queue()\n\tpool = [ multiprocessing.Process(target=pinger, args=(jobs,results)) for i in range(pool_size) ]\n\tfor p in pool:\n\t\tp.start()\n\tfor i in range(1,255):\n\t\tjobs.put('".$ip.".{0}'.format(i))\n\tfor p in pool:\n\t\tjobs.put(None)\n\tfor p in pool:\n\t\tp.join()\n\toutput = ''\n\ta = str(datetime.now());\n\toutput += a[:19]\n\toutput +=' '\n\twhile not results.empty():\n\t\tip = results.get()\n\t\tip += ' '\n\t\toutput +=ip\n\tdb = my.connect(host=\"localhost\",\n\t\t\t\tuser=\"root\",\n\t\t\t\tpasswd=\"mysqleptp\",\n\t\t\t\tdb=\"KLUIOT\"\n\t\t\t\t)\n\tcursor = db.cursor()\n\tid = \"".$labname."\"\n\tnumber_of_rows = cursor.execute('''insert into iptable (labno,online) VALUES(%s,%s)''',(id,output))\n\tdb.commit()\n\tdb.close()";
-fwrite($myfile, $txt);
-fclose($myfile);
-
-
-$myfile1 = fopen("sol1.py", "a");
-echo "";
-$txt = " "." "." "." "."os.system('python /var/www/html/Ippython/".$labname.".py')\n";
-fwrite($myfile1, $txt);
-fclose($myfile1);
-exec("python /var/www/html/Ippython/reboot.py");
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
 }
+
+$conn->close();
+header("Location: index.php#read");
+
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="test.php" method = "POST">
-    NAME:-<input type="text" name="labname">[No-Spaces]<br>
-    RANGE:-<input type="text" name="range">[EX:-10.45.31]<br>
-    <input type="submit" name = "submit">
-    </form>
-</body>
-</html>
--- edit.php
+++ edit.php
@@ -1,26 +1,41 @@
 <?php
+$i=0;
+//echo "asdas";
+//echo $_GET['val'];
 
-if(isset($_POST['submit'])){
+$servername = "localhost";
+$username = "root";
+$password = "mysqleptp";
+$dbname = "KLUIOT";
 
-$ip = $_POST['range'];
-$labname = $_POST['labname'];
-
-$myfile = fopen($labname.".py", "w");
-$i = 0;
-$txt = "from __future__ import print_function\nimport multiprocessing\nimport subprocess\nimport os\nfrom time import gmtime, strftime\nfrom datetime import datetime\nimport MySQLdb as my\ndef pinger( job_q, results_q ):\n\tDEVNULL = open(os.devnull,'w')\n\twhile True:\n\t\tip = job_q.get()\n\t\tif ip is None: break\n\t\ttry:\n\t\t\tsubprocess.check_call(['ping','-c1',ip],stdout=DEVNULL)\n\t\t\tresults_q.put(ip)\n\t\texcept:\n\t\t\tpass\nif __name__ == '__main__':\n\tpool_size = 255\n\tjobs = multiprocessing.Queue()\n\tresults = multiprocessing.Queue()\n\tpool = [ multiprocessing.Process(target=pinger, args=(jobs,results)) for i in range(pool_size) ]\n\tfor p in pool:\n\t\tp.start()\n\tfor i in range(1,255):\n\t\tjobs.put('".$ip.".{0}'.format(i))\n\tfor p in pool:\n\t\tjobs.put(None)\n\tfor p in pool:\n\t\tp.join()\n\toutput = ''\n\ta = str(datetime.now());\n\toutput += a[:19]\n\toutput +=' '\n\twhile not results.empty():\n\t\tip = results.get()\n\t\tip += ' '\n\t\toutput +=ip\n\tdb = my.connect(host=\"localhost\",\n\t\t\t\tuser=\"root\",\n\t\t\t\tpasswd=\"mysqleptp\",\n\t\t\t\tdb=\"KLUIOT\"\n\t\t\t\t)\n\tcursor = db.cursor()\n\tid = \"".$labname."\"\n\tnumber_of_rows = cursor.execute('''insert into iptable (labno,online) VALUES(%s,%s)''',(id,output))\n\tdb.commit()\n\tdb.close()";
-fwrite($myfile, $txt);
-fclose($myfile);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
-$myfile1 = fopen("sol1.py", "a");
-echo "";
-$txt = " "." "." "." "."os.system('python /var/www/html/Ippython/".$labname.".py')\n";
-fwrite($myfile1, $txt);
-fclose($myfile1);
-exec("python /var/www/html/Ippython/reboot.py");
+$sql = "SELECT * FROM TableRange where labname='".$_GET['val']."' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+while($row = $result->fetch_assoc()) {
+    $i = $row['iprange'];
 }
-?>
+}
+$lan = $_GET['val'];
+if(isset($_POST['submit'])){
+$sql = "UPDATE TableRange SET iprange='".$_POST['range']."' WHERE labname='".$_POST['labname']."'";
 
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+header("Location: index.php#read");
+
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,10 +46,18 @@ exec("python /var/www/html/Ippython/reboot.py");
     <title>Document</title>
 </head>
 <body>
-    <form action="test.php" method = "POST">
-    NAME:-<input type="text" name="labname">[No-Spaces]<br>
-    RANGE:-<input type="text" name="range">[EX:-10.45.31]<br>
-    <input type="submit" name = "submit">
+    <form action="edit.php" method="post">
+    <input type="hidden" name="labname" id="labname">    
+    <input type="text" name="range" id="range">
+        <input type="submit" value="submit" name="submit">
     </form>
+
+    <script>
+        var x = "<?php echo $i ?>";
+        var x1 = "<?php echo $lan ?>";
+        document.getElementById("labname").value = x1;
+        document.getElementById("range").value = x;
+    </script>
+
 </body>
 </html>
